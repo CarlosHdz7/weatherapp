7f3ab65d6b2464fca5ef115523b332105e44e04b
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-06-28 22:21:32 -0600

    💄 Add Styles to forecast card

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,7 @@ const lblTemperature = document.getElementById('lblTemperature');
 const lblForecast = document.getElementById('lblForecast');
 const imgForecast = document.getElementById('imgForecast');
 const lblDate = document.getElementById('lblDate');
+const daysSection = document.getElementById('daysSection');
 
 //[FUNCTIONS]
 const getCities = async () => {
@@ -23,10 +24,25 @@ const getCities = async () => {
   console.log(response2);
 
   lblCity.textContent = response2.title;
-  lblTemperature.textContent = response2.consolidated_weather[0].the_temp;
+  lblTemperature.textContent = `${response2.consolidated_weather[0].the_temp.toFixed(2)} °C`;
   lblForecast.textContent = response2.consolidated_weather[0].weather_state_name;
-  imgForecast.src = `https://www.metaweather.com/static/img/weather/${response2.consolidated_weather[0].weather_state_abbr}.svg`;
-  lblDate.textContent = response2.time;
+  imgForecast.src = `https://www.metaweather.com/static/img/weather/png/${response2.consolidated_weather[0].weather_state_abbr}.png`;
+  lblDate.textContent = new Date(response2.time).toLocaleString('en-US');
+
+  let html = '';
+  for(let day of response2.consolidated_weather){
+    console.log(day);
+    html += `
+      <div class="days-section__card shadow">
+          <p>${new Date(day.applicable_date).toLocaleString('en-us', {weekday:'long'})}</p>
+          <img src="https://www.metaweather.com/static/img/weather/${day.weather_state_abbr}.svg" class="mini-img" alt="">
+          <p>${day.the_temp.toFixed(2)} °C</p>
+      </div>
+    `;
+  }
+
+  daysSection.innerHTML = html;
+  
 }
 
 //[EVENTS]
