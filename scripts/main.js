0196fa885d7a1a0c208ef042fe7fddd432e6680f
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-06-28 21:21:18 -0600

    🚧 Demo 2, get forecast info

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,33 @@
-//DEMO
-import {Search} from './search.js';
+'Use strict'
+import { Search } from './Search.js';
 
 //VARIABLES
-let search = new Search()
+let search = new Search();
 
 //[HTML ELEMENTS]
 const txtSearch = document.getElementById('txtSearch');
+const btnSearch = document.getElementById('btnSearch');
+const lblCity = document.getElementById('lblCity');
+const lblTemperature = document.getElementById('lblTemperature');
+const lblForecast = document.getElementById('lblForecast');
+const imgForecast = document.getElementById('imgForecast');
+const lblDate = document.getElementById('lblDate');
 
 //[FUNCTIONS]
-const getCities = async event => {
-  let city = event.target.value;
+const getCities = async () => {
+  let city = txtSearch.value;
   let response = await search.getCities(city);
-  console.log(response);
+  // console.log(response[0].woeid);
+
+  let response2 = await search.getInfo(response[0].woeid);
+  console.log(response2);
+
+  lblCity.textContent = response2.title;
+  lblTemperature.textContent = response2.consolidated_weather[0].the_temp;
+  lblForecast.textContent = response2.consolidated_weather[0].weather_state_name;
+  imgForecast.src = `https://www.metaweather.com/static/img/weather/${response2.consolidated_weather[0].weather_state_abbr}.svg`;
+  lblDate.textContent = response2.time;
 }
 
 //[EVENTS]
-txtSearch.addEventListener('change', getCities);
+btnSearch.addEventListener('click', getCities);
--- scripts/search.js
+++ scripts/search.js
@@ -2,7 +2,22 @@ class Search {
   
   async getCities(city){
     const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://www.metaweather.com/api/location/search/?query=${city}`)}`);
-    if (!response.ok) {    const message = `An error has occured: ${response.status}`;    throw new Error(message);  }
+    if (!response.ok) {   
+      const message = `An error has occured: ${response.status}`;    
+      throw new Error(message); 
+    }
+
+    const data = await response.json();
+    return JSON.parse(data.contents);
+  }
+
+  async getInfo(woeid){
+    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://www.metaweather.com/api/location/${woeid}/`)}`);
+    if (!response.ok) {   
+      const message = `An error has occured: ${response.status}`;    
+      throw new Error(message); 
+    }
+    
     const data = await response.json();
     return JSON.parse(data.contents);
   }
