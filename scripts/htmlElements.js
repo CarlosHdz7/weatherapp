ac44cb94805a9ef7e0100802fd7e279df187d71d
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-07-01 08:56:37 -0600

    🚧 Test fetch without allorigin workaround

---
+++ scripts/htmlElements.js
@@ -0,0 +1,16 @@
+//[HTML ELEMENTS]
+const txtSearch = document.getElementById('txtSearch');
+const itemsFoundContainer = document.getElementById('itemsFoundContainer');
+const lblCity = document.getElementById('lblCity');
+const lblDate = document.getElementById('lblDate');
+const lblTemp = document.getElementById('lblTemp');
+const lblForecast = document.getElementById('lblForecast');
+const imgForecast = document.getElementById('imgForecast');
+const lblSunrise = document.getElementById('lblSunrise');
+const lblSunset = document.getElementById('lblSunset');
+const lblHigh = document.getElementById('lblHigh');
+const lblLow = document.getElementById('lblLow');
+const lblRain = document.getElementById('lblRain');
+const lblWind = document.getElementById('lblWind');
+const cardsContainer = document.getElementById('cardsContainer');
+const inputImg = document.getElementById('inputImg');
--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,9 @@
 'Use strict'
+import './htmlElements.js';
 import { Search } from './Search.js';
 
-//VARIABLES
+//[VARIABLES]
 let search = new Search();
-
-//[HTML ELEMENTS]
-const txtSearch = document.getElementById('txtSearch');
-const itemsFoundContainer = document.getElementById('itemsFoundContainer');
-const lblCity = document.getElementById('lblCity');
-const lblDate = document.getElementById('lblDate');
-const lblTemp = document.getElementById('lblTemp');
-const lblForecast = document.getElementById('lblForecast');
-const imgForecast = document.getElementById('imgForecast');
-const lblSunrise = document.getElementById('lblSunrise');
-const lblSunset = document.getElementById('lblSunset');
-const lblHigh = document.getElementById('lblHigh');
-const lblLow = document.getElementById('lblLow');
-const lblRain = document.getElementById('lblRain');
-const lblWind = document.getElementById('lblWind');
-const cardsContainer = document.getElementById('cardsContainer');
-const inputImg = document.getElementById('inputImg');
 
 //[FUNCTIONS]
 const getCities = async () => {
