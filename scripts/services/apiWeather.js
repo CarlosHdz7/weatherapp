3cf71cb9d1e59df128438089c58d22a1e7318272
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-07-01 10:45:51 -0600

    ⚡️ Add property to ApiWeather class that containt workAround's url

--- scripts/services/apiWeather.js
+++ scripts/services/apiWeather.js
@@ -1,7 +1,11 @@
 export default class ApiWeather {
+
+  constructor(){
+    this.urlWorkAround = 'https://api.allorigins.win/get?url=';
+  }
   
   async getCities(city){
-    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://www.metaweather.com/api/location/search/?query=${city}`)}`);
+    const response = await fetch(`${ this.urlWorkAround }${ encodeURIComponent(`https://www.metaweather.com/api/location/search/?query=${city}`) }`);
     if (!response.ok) {   
       const message = `An error has occured: ${response.status}`;    
       throw new Error(message); 
@@ -12,7 +16,7 @@ export default class ApiWeather {
   }
 
   async getInfo(woeid){
-    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://www.metaweather.com/api/location/${woeid}/`)}`);
+    const response = await fetch(`${ this.urlWorkAround }${ encodeURIComponent(`https://www.metaweather.com/api/location/${woeid}/`) }`);
     if (!response.ok) {   
       const message = `An error has occured: ${response.status}`;    
       throw new Error(message); 
