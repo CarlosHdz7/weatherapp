4beb3829d12000b355f9372916cd7b120ea0dcef
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-07-02 23:59:38 -0600

    ♻️ Improve handling error

--- scripts/services/apiWeather.js
+++ scripts/services/apiWeather.js
@@ -6,22 +6,35 @@ export default class ApiWeather {
   }
   
   async getCities(city) {
+    const ERRORMESSAGE = 'An error has occurred while searching for cities.';
+
     const response = await fetch(`${ this.urlWorkAround }${ encodeURIComponent(`${ this.baseUrlMeta }/api/location/search/?query=${city}`) }`);
     if (!response.ok) {   
-      throw new Error('An error has occurred while searching for cities.'); 
+      throw new Error(ERRORMESSAGE);
     }
-
     const data = await response.json();
+
+    if (data.status.http_code !== 200) {     
+      throw new Error(ERRORMESSAGE); 
+    }
+    
     return JSON.parse(data.contents);
   };
 
   async getCityInfo(woeid) {
+    const ERRORMESSAGE = 'An error has occurred while trying to load the information.';
+    
     const response = await fetch(`${ this.urlWorkAround }${ encodeURIComponent(`${ this.baseUrlMeta }/api/location/${woeid}/`) }`);
     if (!response.ok) {     
-      throw new Error('An error has occurred while trying to load the information.'); 
+      throw new Error(ERRORMESSAGE); 
     }
     
     const data = await response.json();
+
+    if (data.status.http_code !== 200) {     
+      throw new Error(ERRORMESSAGE); 
+    }
+
     return JSON.parse(data.contents);
   };
 
